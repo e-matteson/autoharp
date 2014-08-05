7b1514248e702efed324e02c1dbbcd8417546df7
Emily <e.r.matteson@gmail.com>
2014-08-05 00:59:54 -0400

    slightly better notes

--- muse.py
+++ muse.py
@@ -1,5 +1,16 @@
 import sys, re
 
+
+def print_note(note):
+    note=note.encode('hex')
+    for i in xrange(len(note)):
+        sys.stdout.write(note[i])
+        if i % 4 == 3:
+            print " ",
+    print
+
+
+
 file = open(sys.argv[1], 'rb')
 
 s = file.read()
@@ -8,13 +19,13 @@ note_re = re.compile(".\x40")
 for part in ['treble', 'alto', 'tenor', 'bass']:
     index = note_re.search(s, s.find(part)).start()
     note = s[index:index+29]
+    pitchclass = int(note[0].encode('hex'),16) % 12
+    octave = int(note[0].encode('hex'),16) / 12 - 1
+    print pitchclass
 #    while note data:
  #       parse notes
-    note=note.encode('hex')
-    for i in xrange(len(note)):
-        sys.stdout.write(note[i])
-        if i % 4 == 3:
-            print " ",
-    print
+
+    print_note(note)
     next
+
 
