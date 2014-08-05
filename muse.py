13e634d9cbab75b9b167bab4e8fdc945a92af9b5
Emily <e.r.matteson@gmail.com>
2014-08-05 16:17:17 -0400

    why isn't exporting mxl files trivial?

--- muse.py
+++ muse.py
@@ -1,4 +1,4 @@
-import sys, re
+import sys, re, music21
 
 
 def print_note(note):
@@ -22,21 +22,47 @@ duration_dict={0x27: .256,
                }
                
 
+def htoi(hexstring):
+    return int(hexstring.encode('hex'))
+
+def parse(bytestring):
+    outer_stream = music21.stream.Stream()
+    
+    note_re = re.compile(".\x40")
+    for part in ['treble', 'alto', 'tenor', 'bass']:
+        part_stream = music21.stream.Part()
+        part_stream.id = part.capitalize() #does this need to be unicode?
+        index = note_re.search(bytestring, bytestring.find(part)).start()
+        note = bytestring[index:index+29]
+        while note[-1] == "\x64":
+            try:
+                duration = duration_dict[htoi(note[8:10])]
+            except KeyError:
+                print "unknown duration! " + note[8:10].encode('hex')
+                exit(1)
+
+            if note[0] == "\xff": #Rest
+                m21_rest = music21.note.Rest()
+                m21_rest.duration.quarterlength = duration
+
+                part_stream.append(m21_rest)
+            else:
+                pitchclass = htoi(note[0]) % 12
+                octave = htoi(note[0]) / 12 - 1
+            
+                m21_note = music21.note.Note()
+                m21_note.quarterlength = duration
+                m21_note.octave = octave
+                m21_note.pitch.pitchClass = pitchclass
+
+                part_stream.append(m21_note)
+
+            print_note(note)
+            index += 29
+            note = bytestring[index:index+29]
+        outer_stream.append(part_stream)
+    music21.converter.freeze(outer_stream, fp=sys.argv[1].replace(".mus", ".mxl"))
+
+
 file = open(sys.argv[1], 'rb')
-
-s = file.read()
-
-note_re = re.compile(".\x40")
-for part in ['treble', 'alto', 'tenor', 'bass']:
-    index = note_re.search(s, s.find(part)).start()
-    note = s[index:index+29]
-    pitchclass = int(note[0].encode('hex'),16) % 12
-    octave = int(note[0].encode('hex'),16) / 12 - 1
-    print pitchclass
-#    while note data:
- #       parse notes
-
-    print_note(note)
-    next
-
-
+parse(file.read())
