d5a471c18f7ae4551188a6085e73ca151595eba0
Emily <e.r.matteson@gmail.com>
2014-08-15 06:47:03 -0400

    parsing works!

--- muse.py
+++ muse.py
@@ -1,7 +1,8 @@
 import sys, re, music21
 
 
-def print_note(note):
+def print_note(note, prefix=""):
+    print prefix,
     note=note.encode('hex')
     for i in xrange(len(note)):
         sys.stdout.write(note[i])
@@ -12,6 +13,7 @@ def print_note(note):
 
 duration_dict={0x27: .256,
                0x4e: .5, #eighth
+               0x69: 2./3, 
                0x9d: 1,  #quarter
                0xec: 1.5,
                0x13b: 2, #half
@@ -23,46 +25,83 @@ duration_dict={0x27: .256,
                
 
 def htoi(hexstring):
-    return int(hexstring.encode('hex'))
+    return int(hexstring.encode('hex'),16)
 
 def parse(bytestring):
     outer_stream = music21.stream.Stream()
     
-    note_re = re.compile(".\x40")
-    for part in ['treble', 'alto', 'tenor', 'bass']:
-        part_stream = music21.stream.Part()
-        part_stream.id = part.capitalize() #does this need to be unicode?
-        index = note_re.search(bytestring, bytestring.find(part)).start()
-        note = bytestring[index:index+29]
-        while note[-1] == "\x64":
-            try:
-                duration = duration_dict[htoi(note[8:10])]
-            except KeyError:
-                print "unknown duration! " + note[8:10].encode('hex')
-                exit(1)
+    part_names = ['treble', 'alto', 'tenor', 'bass']
+        
+    part_strings = []
+    for i in xrange(len(part_names) - 1):
+        part_strings.append(bytestring[bytestring.find(part_names[i]):bytestring.find(part_names[i+1])])
 
-            if note[0] == "\xff": #Rest
-                m21_rest = music21.note.Rest()
-                m21_rest.duration.quarterlength = duration
+    end_index = bytestring.find('Tempo')
+    part_strings += [bytestring[bytestring.find(part_names[-1]):(end_index, None)[end_index == -1]]]
 
-                part_stream.append(m21_rest)
-            else:
-                pitchclass = htoi(note[0]) % 12
-                octave = htoi(note[0]) / 12 - 1
+    for part_name, part_string in zip(part_names, part_strings):
+        part_stream = parse_part(part_string, part_name)
+        part_stream.id = part_name.capitalize() #does this need to be unicode
+        outer_stream.insert(0, part_stream)
+    return outer_stream
+
+#    music21.converter.freeze(outer_stream, fp=sys.argv[1].replace(".mus", ".mxl"))
+
+def parse_part(bytestring, part):
+    print "*********\n"+part
+    part_stream = music21.stream.Part()
+    note_re = re.compile(".\x40.{12}\xff\xff") #regex for finding notes
+    
+    num_notes = 0
+    chord_note = None
+
+    notes = note_re.findall(bytestring)
+
+    for note in notes:
+        print_note(note, str(num_notes)+"\t")
+        try:
+                #duration stored in bytes 6 and 7 
+            duration = duration_dict[htoi(note[6:8])]
+        except KeyError:
+            print "unknown duration: " + note[6:8].encode('hex')
+            print part + ", note # " + str(num_notes)
+            print_note(note)
+            sys.exit(1)
             
-                m21_note = music21.note.Note()
-                m21_note.quarterlength = duration
-                m21_note.octave = octave
-                m21_note.pitch.pitchClass = pitchclass
+        if note[0] == "\xff": #Rest
+            m21_rest = music21.note.Rest()
+            m21_rest.duration.quarterLength=duration
+            part_stream.append(m21_rest)
+        else: #is note
+            pitchclass = htoi(note[0]) % 12
+            octave = htoi(note[0]) / 12 - 1
+            m21_note = music21.note.Note(quarterLength=duration)
+#                m21_note.quarterlength = duration
+            m21_note.octave = octave
+            m21_note.pitch.pitchClass = pitchclass
 
+            if htoi(note[3]) in (1,5):
+#                print "found chord! " + part + " "+str(num_notes)
+                chord_note = m21_note
+            else:
+                if chord_note != None:
+                    if duration == chord_note.quarterLength:
+                        m21_note=music21.chord.Chord([chord_note, m21_note])
+                        chord_note = None
+                    else:
+                        print "Chord with notes of different durations!"
+                        print_note(note)
                 part_stream.append(m21_note)
 
-            print_note(note)
-            index += 29
-            note = bytestring[index:index+29]
-        outer_stream.append(part_stream)
-    music21.converter.freeze(outer_stream, fp=sys.argv[1].replace(".mus", ".mxl"))
+#            if part=='bass' and num_notes < 10:
+#                print_note(note)
+
+        num_notes += 1
+
+    return part_stream
+
 
 
 file = open(sys.argv[1], 'rb')
-parse(file.read())
+outer_stream = parse(file.read())
+outer_stream.show()
